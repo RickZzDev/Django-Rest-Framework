a47e12d5f633c9328f18248b1e2ef6157c6f06dc
Carlos Henrique <leftwoolf@gmail.com>
2020-02-07 11:58:11 -0300

    subindo dnv

--- api/core/views.py
+++ api/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Empresas
 from .serializers import EmpresaSerializer
 
@@ -32,11 +32,28 @@ from rest_framework.response import Response
 
 
 # Create your views here.
+# A EMPRESA VIEW SET É COMO SE FOSSER UMA CONTROLLER 
+# ELA IDENTIFICA QUAL A AÇÃO NECESSARIA
+# CHAMA O MODEL NECESSARIO
+# E O SERIALIZER PARA TRANSFORMAR ESTE RETORNO EM JSON
 class EmpresaViewSet(viewsets.ModelViewSet):
-        queryset = Empresas.objects.all()
-        serializer_class = EmpresaSerializer
+
+        
+        # ESTE METODO IRA LISTAR TODOS OS REGISTROS
+        def list(self, request):
+            queryset = Empresas.objects.all()
+            serializer_class = EmpresaSerializer(queryset, many=True)
+            return Response(serializer_class.data)
             
+        # ESTE METODO IRA MOSTRAR UM UNICO REGISTO
+        def retrieve(self,request, pk=None):
+            queryset = Empresas.objects.all()
+            empresa = get_object_or_404(queryset, pk=pk)
+            serializer_class = EmpresaSerializer(empresa)
+            return Response(serializer_class.data)
 
-#
-
-    
+        def create(self, request):
+            serializer = EmpresaSerializer(data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                 
